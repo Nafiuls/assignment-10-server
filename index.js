68b9dd697290b9a712b9017398b59c6942551068
Nafiuls <snafiul700@gmail.com>
2025-05-14 12:03:57 +0600

    almost done Alhamdulillah

--- index.js
+++ index.js
@@ -31,20 +31,41 @@ async function run() {
     const reviews = chillGame.collection("reviews");
     const watchList = chillGame.collection("watchList");
 
-    // POST a data
+    // POST a review data
     app.post("/add-review", async (req, res) => {
       const data = req.body;
       const result = await reviews.insertOne(data);
       res.send(result);
     });
 
-    // get all data
+    // edit a review
+    app.patch("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviews.updateOne(query, {
+        $set: updatedData,
+      });
+      res.send(result);
+    });
+
+    // get all review data
     app.get("/reviews", async (req, res) => {
       const data = await reviews.find().toArray();
       res.send(data);
     });
 
-    // find single data
+    // highest rated review games
+    app.get("/highest", async (req, res) => {
+      const data = await reviews
+        .find()
+        .sort({ gameRating: -1 })
+        .limit(6)
+        .toArray();
+      res.send(data);
+    });
+
+    // find single review data
     app.get("/reviews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -52,11 +73,26 @@ async function run() {
       res.send(result);
     });
 
-    // delete a data
+    // delete a review data
     app.delete("/review-delete/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await reviews.deleteOne(query);
+      res.send(result);
+    });
+
+    // get review data with user email
+    app.get("/review/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await reviews.find(query).toArray();
+      res.send(result);
+    });
+
+    // POST a watchlist data
+    app.post("/add-watchlist", async (req, res) => {
+      const data = req.body;
+      const result = await watchList.insertOne(data);
       res.send(result);
     });
 
