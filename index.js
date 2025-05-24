fd7c6bd550cd6e686ffc14b60919dcc96b999ce3
Nafiuls <snafiul700@gmail.com>
2025-05-24 13:43:47 +0600

    final push

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 5000;
 
 // middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s5vxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
